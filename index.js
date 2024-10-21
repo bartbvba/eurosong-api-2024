699d5ec3095e6916649d2e97f84c35a0f7ff6318
Bartbvbva <robbenollet1@gmail.com>
2024-10-21 12:30:08 +0200

    added the api/artists

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get('/', (req, res) => {
     res.send('Hello World! ');
 });
 
+app.get('/api/artists', (req, res) => {
+    res.send([
+        "JB",
+        "Beyonce",
+    ]);
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-});
+})
